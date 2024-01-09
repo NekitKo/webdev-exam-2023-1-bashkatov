73ecc509261839da38768d2957d7cb74954c40f7
NekitKo <h.hoverq@yandex.ru>
2024-01-09 19:27:37 +0300

    final version I guess

--- exam/main.js
+++ exam/main.js
@@ -1,64 +1,102 @@
 'use strict';
 const url = new URL("http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=26a75d98-76bf-48a8-9acd-e75e991c56cb");
+const pang = {
+   page: 1,
+   finp: 4,
+   counter: 0,
+};
 function getData() {
-    var xhr = new XMLHttpRequest();
+   var xhr = new XMLHttpRequest();
    
-    // Запрос GET к серверу
-    xhr.open('GET', url, true);
+   // Запрос GET к серверу
+   xhr.open('GET', url, true);
    
-    // Обработка ответа от сервера
-    xhr.onload = function() {
-       if (xhr.status === 200) {
+   // Обработка ответа от сервера
+   xhr.onload = function() {
+      if (xhr.status === 200) {
          // Парсинг JSON-объекта
          var data = JSON.parse(xhr.responseText);
-   
+
          // Вывод информации на страницу
          displayData(data);
-       } else {
+      } else {
          console.error('Ошибка:', xhr.status);
-       }
-    };
+      }
+   };
    
-    // Обработка ошибок
-    xhr.onerror = function() {
-       console.error('Ошибка при запросе данных');
-    };
+   // Обработка ошибок
+   xhr.onerror = function() {
+      console.error('Ошибка при запросе данных');
+   };
    
-    // Отправка запроса
-    xhr.send();
+   // Отправка запроса
+   xhr.send();
 }
    
 // Функция для вывода информации на страницу
 function displayData(data) {
-    var table = document.getElementById('path');
-    var tbody = table.getElementsByTagName('tbody')[0];
+   var table = document.getElementById('path');
+   var tbody = table.getElementsByTagName('tbody')[0];
    
-    // Очистка таблицы от старых данных
-    while (tbody.firstChild) {
-       tbody.removeChild(tbody.firstChild);
-    }
+   // Очистка таблицы от старых данных
+   while (tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+   }
    
-    // Цикл по каждому элементу данных
-    for (var i = 0; i < data.length; i++) {
-       var row = document.createElement('tr');
-       var nameCell = document.createElement('td');
-       var descriptionCell = document.createElement('td');
-       var mainObjectCell = document.createElement('td');
-       var button = document.createElement('button');
+   clear(tbody);
 
-       nameCell.textContent = data[i].name;
-       descriptionCell.textContent = data[i].description;
-       mainObjectCell.textContent = data[i].mainObject;
-       button.textContent = 'Выбрать';
+   pang.counter = Math.ceil(data.length / pang.finp);
+   var st = (pang.page - 1) * pang.finp;
+   var end = st + pang.finp;
+   var displayedData = data.slice(st, end);
 
-       row.appendChild(nameCell);
-       row.appendChild(descriptionCell);
-       row.appendChild(mainObjectCell);
-       row.appendChild(button);
+   // Цикл по каждому элементу данных
+   for (var i = 0; i < displayedData.length; i++) {
+      var obj = displayedData[i];
+      var row = tbody.insertRow();
 
-       tbody.appendChild(row);
-    }
+      var eleme = row.insertCell();
+      eleme.innerHTML = obj.name;
+      
+      var elem = row.insertCell();
+      elem.innerHTML = obj.description;
+      
+      var ele = row.insertCell();
+      var el = row.insertCell();
+      
+      var mainObject = obj.mainObject;
+      var mainObjectCell = document.createElement('ul');
+      var list = document.createElement('list');
+      list.textContent = mainObject;
+      mainObjectCell.appendChild(list);      
+      ele.appendChild(mainObjectCell);
+      
+      var btn = document.createElement('button');
+      btn.textContent = 'Выбрать';
+      el.appendChild(btn);
+   }
+      
+}
+
+function clear(body){
+   body.innerHTML = '';
+}
+
+function getNextPage() {
+   if (pang.page < pang.counter) {
+      pang.page++;
+      getData();
+   }
+}
+      
+function getBackPage() {
+   if (pang.page > 1) {
+      pang.page--;
+      getData();
+   }
 }
 
 // Вызов функции для получения данных
-getData();
+window.onload = () => {
+   getData();
+};
